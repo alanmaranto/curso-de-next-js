831d4f9a962ae9ce43bb4b9e667fe09b62a29289
alan <alanarostegui@hotmail.com>
2021-10-14 01:02:44 -0500

    style: delete styles and add smeantic ui css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import CartProvider from "@store/Cart";
 import { AppProps } from "next/app";
 
-import "semantic-ui-react";
-import "../styles/main.scss";
+import 'semantic-ui-css/semantic.min.css'
+import '../global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // providers -- Context, theme, language, data
