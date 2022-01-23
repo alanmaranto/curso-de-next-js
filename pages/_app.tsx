5463d8ad93f949787f8f1689168cfbe356810cc1
alan <alanarostegui@hotmail.com>
2022-01-23 02:24:19 -0600

    feat: disable reportwebvitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,11 @@ import { AppProps } from "next/app";
 import "semantic-ui-css/semantic.min.css";
 import "../global.css";
 
-export function reportWebVitals(metric) {
+// export function reportWebVitals(metric) {
   // console.log(metric);
   // analytics - calibre
   // serverAnalytics.log(metric)
-}
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   // providers -- Context, theme, language, data
